5fd0e628e4d1f53e5c81f6615427222ed97cae22
ShuzoN <d.namu.r21@gmail.com>
2016-08-10 00:22:26 +0900

    [mod] validateとcancancanの設定

--- app/models/assign_group_place.rb
+++ app/models/assign_group_place.rb
@@ -1,4 +1,6 @@
 class AssignGroupPlace < ActiveRecord::Base
   belongs_to :place_order
   belongs_to :place
+  validates_presence_of :place_order_id, :place_id
+  validates :place_order_id, uniqueness: {scope: [:place_id] }
 end
