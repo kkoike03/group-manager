0f8fb26e8f09611cc0c87fb9ac41d6b77413772d
shuzon <d.namu.r21@gmail.com>
2017-05-20 12:07:49 +0900

    group詳細にuserの情報を表示

--- app/admin/group.rb
+++ app/admin/group.rb
@@ -1,7 +1,15 @@
 ActiveAdmin.register Group do
-
   permit_params :user_id, :name, :group_category_id, :activity, :first_question,
                 :fes_year_id
+
+  controller do
+    def show
+      group = Group.find(params[:id])
+      params[:user] = UserDetail.find_by(user_id: group.user_id)
+      params[:subrep] = SubRep.find_by(group_id: group.id)
+      show!
+    end
+  end
 
   index do
     selectable_column
@@ -31,5 +39,24 @@ ActiveAdmin.register Group do
     column :activity
     column :created_at
     column :updated_at
+  end
+
+  show do
+    attributes_table do
+      row :id
+      row :name
+      row :group_category_id
+      row :user_name do |_| params[:user] end
+      row :user_id
+      row :user_tel do |_| params[:user].tel end
+      row :subrep_name do |_| params[:subrep].name_ja end
+      row :subrep_tel  do |_| params[:subrep].tel end
+      row :activity
+      row :first_question
+      row :created_at
+      row :updated_at
+      row :fes_year_id
+    end
+    active_admin_comments
   end
 end
